939e6540b40d27a07dbcf855a9fd28cbd8cac973
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-14 11:53:26 -0500

    FizzBuzz solution

---
+++ fizzBuzz.js
@@ -0,0 +1,21 @@
+/***************************
+ * PART 1: Fizz Buzz       * 
+ * Name: Richard Tillies   *
+ * Date: February 15, 2024 *
+ ***************************/
+
+const MAX_NUMBER = 100
+const FIZZ = "Fizz"
+const BUZZ = "Buzz"
+const FIZZBUZZ = "Fizz Buzz"
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0)
+    console.log(FIZZBUZZ);
+  else if (i % 3 == 0)
+    console.log(FIZZ);
+  else if (i % 5 == 0)
+    console.log(BUZZ);
+  else
+    console.log(i);
+}
